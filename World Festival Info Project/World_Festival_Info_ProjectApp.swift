ccf19aa78bad27aaf815a46a9d146516a1dc6fab
ByungMoon-Kim <darkyu457@naver.com>
2025-06-12 00:39:42 +0900

    Initial Commit

---
+++ World Festival Info Project/World_Festival_Info_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_Festival_Info_ProjectApp.swift
+//  World Festival Info Project
+//
+//  Created by ByungMoon Kim on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct World_Festival_Info_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
