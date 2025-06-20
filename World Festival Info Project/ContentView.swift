f72d485efa07975fbfdba1cad11ba175274fdc23
ByungMoon-Kim <darkyu457@naver.com>
2025-06-21 02:32:56 +0900

    검색박스 추가

--- World Festival Info Project/ContentView.swift
+++ World Festival Info Project/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            TextField("검색어를 입력하세요.", text: $searchText)
+                .padding(10)
+                .background(Color(.systemGray4))
+                .cornerRadius(8)
+                .padding([.top, .horizontal])
+                .multilineTextAlignment(.center)
+
+            Text("검색어: \(searchText)")
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.white)
     }
 }
 
