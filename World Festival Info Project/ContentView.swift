26941e114f1c347e44e89410ca9efdee68f680c5
ByungMoon-Kim <darkyu457@naver.com>
2025-06-25 00:31:41 +0900

    주석 추가 및 타이틀 추가

--- World Festival Info Project/ContentView.swift
+++ World Festival Info Project/ContentView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by ByungMoon Kim on 6/12/25.
 //
+// aligment 이거는 정렬 기준을 말한다.
 
 import SwiftUI
 
@@ -17,16 +18,28 @@ struct ContentView: View {
                 .background(Color(.systemGray4))
                 .cornerRadius(8)
                 .padding([.top, .horizontal])
-                .multilineTextAlignment(.center)
+                    // 검색창이 화면 위에 붙지 않게 여백 주기
+                .multilineTextAlignment(.leading)
+                    // 텍스트를 왼쪽 정렬
 
             Text("검색어: \(searchText)")
                 .frame(maxWidth: .infinity)
+                    // 가로를 꽉 채워서
                 .multilineTextAlignment(.center)
+                    // 텍스트를 가운데 정렬
             
             Spacer()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white)
+        VStack(alignment: .center) {
+            Text("World Festival")
+                .font(.largeTitle)
+                .padding()
+                // 텍스트가 너무 위에 딱 붙지 않게 여백 줌
+            Spacer()
+                // 아래에 공간을 만들어서 Text를 위로 밀어줌
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(Color.white)
     }
 }
 
